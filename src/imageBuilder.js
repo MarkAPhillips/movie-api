a4a58fdc5f1a536f3376150d37ece55563d8742f
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-05-02 22:14:22 +0100

    Initial fix for caching and use async/await

--- src/imageBuilder.js
+++ src/imageBuilder.js
@@ -1,41 +1,48 @@
-import memCache from 'memory-cache';
+import NodeCache from 'node-cache';
 import fetch from 'node-fetch';
 import { ENV_VARS, BASE_URL } from './constants';
 import { handleResponse, handleError } from './fetchHandler';
 
-const DEFAULT_IMAGE_SIZE = 'w92';
+const DEFAULT_IMAGE_SIZE = 'w154';
+const CACHE_KEY = '__movie_api_config__';
+const cache = new NodeCache();
 
-const configuration = () => {
+// eslint-disable-next-line consistent-return
+const configuration = async () => {
   const url = `${BASE_URL}/configuration?api_key=${ENV_VARS.API_KEY}`;
   console.log(`Connecting to ${url}`);
-  return fetch(url)
-    .then(handleResponse)
-    .then(({ images }) => ({
+  try {
+    const response = await fetch(url);
+    const { images } = await handleResponse(response);
+    return ({
       imageBaseUrl: images.secure_base_url,
       imagePosterSizes: images.poster_sizes,
-    }))
-    .catch((err) => handleError(err, url));
+    });
+  } catch (err) {
+    handleError(err, url);
+  }
 };
 
 /*
-Structure of image https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+Structure of image https://image.tmdb.org/t/p/w154/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
 */
-const buildImage = async (posterPath, key) => {
-  const cachedResponse = memCache.get(key);
-  if (cachedResponse) return cachedResponse;
+const buildImage = async (posterPath) => {
+  let cachedImageConfiguration = cache.get(CACHE_KEY);
   try {
-    const imageConfiguration = await configuration();
-    const { imageBaseUrl, imagePosterSizes } = imageConfiguration;
+    if (!cachedImageConfiguration) {
+      cachedImageConfiguration = await configuration();
+      cache.set(CACHE_KEY, cachedImageConfiguration);
+    }
+    const { imageBaseUrl, imagePosterSizes } = cachedImageConfiguration;
     const imageSize = imagePosterSizes.find((item) => item === DEFAULT_IMAGE_SIZE);
     if (imageSize || !posterPath) {
-      memCache.put(key, `${imageBaseUrl}${imageSize}${posterPath}`);
-      return memCache.get(key);
+      return `${imageBaseUrl}${imageSize}${posterPath}`;
     }
     console.log(`Image size ${DEFAULT_IMAGE_SIZE} not found in ${imagePosterSizes.toString()}`);
   } catch (err) {
     console.log(`An Unhandled exception occurred ${err}`);
   }
-  return 'https://via.placeholder.com/92'; // TODO: Review placeholder image size
+  return 'https://via.placeholder.com/154'; // TODO: Review placeholder image size
 };
 
 export default buildImage;
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,32 +1,46 @@
+/* eslint-disable consistent-return */
 import fetch from 'node-fetch';
 import { ENV_VARS, BASE_URL } from './constants';
 import { handleResponse, handleError } from './fetchHandler';
 import buildImage from './imageBuilder';
 
-export const trending = () => {
+export const trending = async () => {
   const url = `${BASE_URL}/trending/movie/day?api_key=${ENV_VARS.API_KEY}`;
   console.log(`Connecting to ${url}`);
-  return fetch(url)
-    .then(handleResponse)
-    .then(({ results }) => results.map((item) => ({
-      id: item.id,
-      title: item.title,
-      overview: item.overview,
-      imageUrl: buildImage(item.poster_path, '__movie_api_trending__'),
-    })))
-    .catch((err) => handleError(err, url));
+  try {
+    const response = await fetch(url);
+    const { results } = await handleResponse(response);
+    const output = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const result of results) {
+      // eslint-disable-next-line no-await-in-loop
+      const imageUrl = await buildImage(result.poster_path);
+      output.push({
+        id: result.id,
+        title: result.title,
+        overview: result.overview,
+        imageUrl,
+      });
+    }
+    return output;
+  } catch (err) {
+    handleError(err, url);
+  }
 };
 
-export const popular = () => {
+export const popular = async () => {
   const url = `${BASE_URL}/movie/popular?api_key=${ENV_VARS.API_KEY}&language=en-US&page=1`;
   console.log(`Connecting to ${url}`);
-  return fetch(url)
-    .then(handleResponse)
-    .then(({ results }) => results.map((item) => ({
+  try {
+    const response = await fetch(url);
+    const { results } = await handleResponse(response);
+    return results.map((item) => ({
       id: item.id,
       title: item.title,
       overview: item.overview,
       voteAverage: item.vote_average,
-    })))
-    .catch((err) => handleError(err, url));
+    }));
+  } catch (err) {
+    handleError(err, url);
+  }
 };
