5d5c71125f3779e70409335ef95bfe242ea6539d
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-05-28 09:29:27 +0100

    Fix path issues

--- graphql/index.js
+++ graphql/index.js
@@ -24,7 +24,7 @@ const resolvers = {
 };
 
 const server = new ApolloServer({
-  typeDefs: importSchema('src/schema.graphql'),
+  typeDefs: importSchema('graphql/schema.graphql'),
   resolvers,
 });
 
