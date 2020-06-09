16f947ac74e186791d85f18838b47f916fcb6763
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-06-09 15:59:03 +0100

    Add person builder

---
+++ services/builders/personBuilder.test.js
@@ -0,0 +1,46 @@
+import personBuilder from './personBuilder';
+import get from '../restService';
+
+const url = 'https://api.themoviedb.org/3/person/59675';
+
+const mockData = {
+  birthday: '1973-06-12',
+  known_for_department: 'Acting',
+  deathday: null,
+  id: 59675,
+  name: 'Mel Rodriguez',
+  also_known_as: [],
+  gender: 2,
+  biography: 'Robert Michael Morris is an American actor.',
+  popularity: 3.168,
+  place_of_birth: 'Miami, Florida, USA',
+  profile_path: '/wwvthcEDo6WHOHumcoh1tT1IaoB.jpg',
+  adult: false,
+  imdb_id: 'nm0735440',
+  homepage: null,
+};
+
+// mocks
+jest.mock('../restService');
+get.mockImplementation(() => Promise.resolve(mockData));
+
+jest.mock('./imageBuilder', () => ({
+  buildImage: jest.fn(() => Promise.resolve('https://image.tmdb.org/t/p/w154/wwvthcEDo6WHOHumcoh1tT1IaoB.jpg')),
+}));
+
+describe('personBuilder tests', () => {
+  it('should return a person', async () => {
+    const expected = {
+      id: 59675,
+      name: 'Mel Rodriguez',
+      imageUrl: 'https://image.tmdb.org/t/p/w154/wwvthcEDo6WHOHumcoh1tT1IaoB.jpg',
+      biography: 'Robert Michael Morris is an American actor.',
+      birthDate: '1973-06-12',
+      deathDate: null,
+      placeOfBirth: 'Miami, Florida, USA',
+    };
+    const person = await personBuilder(url, 'w154');
+    expect(get).toHaveBeenCalledWith(url);
+    expect(person).toEqual(expected);
+  });
+});
